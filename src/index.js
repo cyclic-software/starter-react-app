d746a784c90ddbc9fe9d9291e9854e6fab14a5d1
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-10-21 17:05:36 -0600

    correcting outdated import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ReactDOM } from 'react';
 import './index.css';
 import App from './App';
 import UserProvider from './context/UserProvider'
