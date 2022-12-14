d991b712ba1023df6356a4bfa4b83ed9cac3f53a
dwibambang72 <72191762+dwibambang72@users.noreply.github.com>
2022-12-14 09:49:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Test
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
