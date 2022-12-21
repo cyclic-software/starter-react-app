4474003878d66f15759e1fdb32ac0ddcb3000e48
NADUNSANDEEPA <nadun2952@gmail.com>
2022-12-21 10:12:44 +0530

    one

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Nadun
+          Learn React Nadun Sandeepa
         </a>
       </header>
     </div>
