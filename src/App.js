bfbc814b44cb8205486a7411875c9bb675034af0
NADUNSANDEEPA <48161752+NADUNSANDEEPA@users.noreply.github.com>
2022-12-21 10:10:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Nadun
         </a>
       </header>
     </div>
