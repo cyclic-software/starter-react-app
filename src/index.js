0827708ad8743df692714d88b8a287c610d458f0
unknown <Student@IT-STULOAN-1550.csun.edu>
2022-12-27 22:50:59 -0800

    Fixed error

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 
