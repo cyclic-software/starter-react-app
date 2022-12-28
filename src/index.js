8af128d1a30fab0f07fc0c5c9442efbda98bf650
unknown <Student@IT-STULOAN-1550.csun.edu>
2022-12-27 22:38:30 -0800

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
 
