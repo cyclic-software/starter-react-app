64fc98c0dbc06a57ac6533a78b0b0f7df85ae8c0
SANDIP DAS <108420428+sandip2233@users.noreply.github.com>
2023-05-23 12:53:00 +0000

    Pending changes exported from your codespace

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
