ffc0da0e988fa723783915d131c688e67e40ccfb
Andrii Nasadiuk <123100628+AndriiNasadiuk@users.noreply.github.com>
2023-07-30 17:58:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         I have deployed React project on PaaS system.
         </p>
         <a
           className="App-link"
