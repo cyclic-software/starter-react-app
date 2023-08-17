15186a70a3c0916099ec52a1e9a78b405cef9221
arveymenon <arveymenon@gmail.com>
2023-08-17 22:52:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let generateText = async () => {
+    console.log("generate logo");
+
+    fetch("/generateBlog", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "hey":"yo"
+        })
+    }).then(res=>{
+        console.log("Response", res)
+    })
+  }
+
+  // let generateLogo = async () => {
+
+  // }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <button onClick={generateText}>Generate Blog</button>
+
+
+
+          {/* <button onClick={generateLogo}>Generate Logo</button> */}
+
+        </header>
+      </div>
+    );
 }
 
 export default App;
