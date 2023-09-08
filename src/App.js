1dd8c66270c4eefcedafb15033df6629b63114ee
yash-ak <133066101+yash-ak@users.noreply.github.com>
2023-09-08 11:34:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Yash A. Akbari :-) 126403229 ( Software Developement )
         </p>
         <a
           className="App-link"
