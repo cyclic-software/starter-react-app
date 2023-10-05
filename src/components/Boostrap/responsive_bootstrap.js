e79de4c2da950fcb4056be17c9e9f25a048f3ea3
Marcelle <yemebelmarcelle@gmail.com>
2023-10-05 17:30:29 +0200

    deploy

---
+++ src/components/Boostrap/responsive_bootstrap.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Mon Portfolio</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Section 1</h2>
+          {/* Contenu de la section 1 */}
+        </div>
+        <div className="col-md-6">
+          <h2>Section 2</h2>
+          {/* Contenu de la section 2 */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
