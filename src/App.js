95ba9ce192cbdc44b8aeb4ea4f8bb47d9438ba8d
Viktor <122560983+Careless-boop@users.noreply.github.com>
2023-11-14 02:30:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Viktor <code>Halushka</code>.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React\n(Вчіть реакт)
         </a>
       </header>
     </div>
