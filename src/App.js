4af470546647c78f9415f60db40943b6d7f1cb6e
Vera Odnoralova <103371149+architectVera@users.noreply.github.com>
2024-01-02 14:55:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Vera Odnoralova
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
